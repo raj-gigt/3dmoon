701cebb557e88f70c2bcc4055beccfc0d88b828f
RAJAT DUGAR <rajat02514@gmail.com>
2024-12-07 23:53:00 +0530

    it fuckin works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-import flowbite from "flowbite-react/tailwind.js";
+const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", flowbite.content],
-  theme: {
-    extend: {},
-  },
-  plugins: [flowbite.plugin],
+module.exports = {
+  content: [
+    // ...
+    flowbite.content(),
+  ],
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
 };
